6f5d85ad811388aa21f4a36cebd3723337249050
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2026-07-13 03:03:30 +0600

    fix: update ESLint configuration and package.json for improved linting and project name

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,18 +1,21 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
 
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@next/next/no-img-element": "off",
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@next/next/no-img-element': 'off',
+
+      // Husky will fail on these
+      'no-trailing-spaces': 'error',
+      'no-multi-spaces': 'error',
     },
   },
 ]);
-
-export default eslintConfig;
